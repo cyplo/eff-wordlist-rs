82cab02e2de6fbf871559d65ff221b8553c6149d
Cyryl Płotnicki <cyplo@cyplo.net>
2019-02-10 12:17:04 +0000

    Expose large wordlist

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+mod eff_large_wordlist;
+use eff_large_wordlist::LARGE;
+
+pub mod large {
+    use crate::LARGE;
+    use rand::rngs::OsRng;
+    use rand::seq::SliceRandom;
+
+    pub fn random_word() -> &'static str {
+        let mut rng = OsRng::new().expect("Not able to operate without random source.");
+        LARGE.choose(&mut rng).unwrap().1
     }
+
+    
 }
