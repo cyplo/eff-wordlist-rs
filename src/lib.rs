13e62766565278eb00a93c5f92ee93ee52301abd
Cyryl Płotnicki <cyplo@cyplo.net>
2019-02-10 12:27:19 +0000

    Add basic documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,19 @@
-mod eff_large_wordlist;
-use eff_large_wordlist::LARGE;
+//! This crate exposes the EFF wordlists under a Rust API.
+//! See EFF's [blog post](https://www.eff.org/deeplinks/2016/07/new-wordlists-random-passphrases) for more information about the wordlists.
 
+mod eff_large_wordlist;
+
+/// large wordlist
 pub mod large {
-    use crate::LARGE;
     use rand::rngs::OsRng;
     use rand::seq::SliceRandom;
 
+    /// The wordlist as an array of tuples - the first element being the dice roll and second the equivalent word
+    pub const LIST: &'static [(u32, &'static str)] = crate::eff_large_wordlist::LARGE;
+
     pub fn random_word() -> &'static str {
         let mut rng = OsRng::new().expect("Not able to operate without random source.");
-        LARGE.choose(&mut rng).unwrap().1
+        LIST.choose(&mut rng).unwrap().1
     }
 
 
